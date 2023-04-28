a26efe6ad59914ca39129ad4638e1678d4b4ee99
marble <git@computer-in.love>
2023-04-28 19:30:49 +0200

    use tar ball instead of git clone

--- shell.nix
+++ shell.nix
@@ -1,10 +1,5 @@
 { pkgs ? import
-    (builtins.fetchGit {
-      name = "nixos-unstable-2023_02_25";
-      url = "https://github.com/nixos/nixpkgs/";
-      ref = "refs/heads/nixos-unstable";
-      rev = "988cc958c57ce4350ec248d2d53087777f9e1949";
-    })
+    (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/988cc958c57ce4350ec248d2d53087777f9e1949.tar.gz")
     { }
 }:
 
